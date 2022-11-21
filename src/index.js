c4375c8b9d6d21b5e40e9e73cd762f699e43fa54
Iryna-Somyuk <Iryna.Somuyk@gmail.com>
2022-11-21 21:06:51 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 //console.log(input);
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch() {
+    const evtValue = input.value.trim();
+    //console.log(evtValue);
+    searchCountries(evtValue).then(data => createCountriesMarkUp(data))
+   
+}
+function searchCountries(name) {
+ const BASE_URL = 'https://restcountries.com/v3.1/name/';
+   fetch(`${BASE_URL}${name}?fields=name,capital,currencies,population,flags.svg,languages`)
+   .then(resp => {
+    if(!resp.ok) {
+        thow new Error(resp.stu)
+    }
+   })
+}
